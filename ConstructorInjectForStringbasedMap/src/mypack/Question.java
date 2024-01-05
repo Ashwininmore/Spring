e92a05ffafce7727f5a51a1e7b93a1dd42a5ad41
Ashwini Nitin More <69009147+Ashwininmore@users.noreply.github.com>
2024-01-05 15:54:48 +0530

    Constructor-Injection-For-Stringbased-Map

---
+++ ConstructorInjectForStringbasedMap/src/mypack/Question.java
@@ -0,0 +1,22 @@
+package mypack;
+
+import java.util.Map;
+
+public class Question {
+	private int qid;
+	private String qname;
+	private Map<String, String> ans;
+	public Question(int qid, String qname, Map<String, String> ans) {
+		super();
+		this.qid = qid;
+		this.qname = qname;
+		this.ans = ans;
+	}
+	
+	public void display() {
+		System.out.println("Question Id:"+qid+"\nQuestion name="+qname+"\nAnswers=");
+		for(Map.Entry<String, String>e: ans.entrySet()) {
+			System.out.println("Answer="+e.getKey()+"postedby="+e.getValue());
+		}
+	}
+}
---
+++ ConstructorInjectForStringbasedMap/src/mypack/QuestionBank.java
@@ -0,0 +1,15 @@
+package mypack;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class QuestionBank {
+
+	public static void main(String[] args) {
+			ApplicationContext ctx= new ClassPathXmlApplicationContext("applicationContext.xml");
+			Question q1=(Question)ctx.getBean("q1");
+			q1.display();
+
+	}
+
+}
